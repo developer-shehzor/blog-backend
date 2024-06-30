02fd479bc59eb35bee8dc82f49f7b906b89c9ff9
ShehzorMemon <Shezymemon0180@gmail.com>
2024-06-30 22:43:25 +0500

    Completed logout functionality

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const User = require('./models/User')
 const bcrypt = require('bcryptjs')
 const app = express()
 const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 
 const salt = bcrypt.genSaltSync(10)
 const secret = 'ahaibhjkazcv2566haj89yaah'
 
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
 app.use(express.json())
+app.use(cookieParser())
+
 
 mongoose.connect('mongodb+srv://shezymemon0180:gu6c0Sok1EW8ZH3i@cluster0.wxeibnz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
@@ -29,19 +32,49 @@ app.post('/register', async (req, res) => {
 })
 
 app.post('/login', async (req, res) => {
-    const {username, password} = req.body;
-    const userDoc = await User.findOne({username})
-    const passOk = bcrypt.compareSync(password, userDoc.password)
-    if (passOk) {
-        // logged in
-        jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
-            if (err) throw err;
-            res.cookie('token', token).json('ok');
-        })
-        
-    } else {
-        res.status(400).json('wrong credentials')
+    const { username, password } = req.body;
+
+    try {
+        const userDoc = await User.findOne({ username });
+
+        if (!userDoc) {
+            return res.status(400).json('User not found'); // Return early if user does not exist
+        }
+
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+
+        if (passOk) {
+            // logged in
+            jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+                if (err) throw err;
+                res.cookie('token', token).json({
+                    id: userDoc._id,
+                    username,
+                });
+            });
+        } else {
+            res.status(400).json('Wrong credentials');
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).json('Internal Server Error');
     }
+});
+
+
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) {
+            console.error(err);
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+        res.json(info);
+    });
+});
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok')
 })
 
 app.listen(4000)
